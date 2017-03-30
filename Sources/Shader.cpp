42379952032393ffeae2596d86c5e2b824bb20b0
Robert Konrad <git2016@robdangero.us>
2017-03-30 16:56:54 +0200

    Update Kore

--- Sources/Shader.cpp
+++ Sources/Shader.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 
 #include <Kore/IO/FileReader.h>
-#include <Kore/Graphics/Graphics.h>
-#include <Kore/Graphics/Shader.h>
+#include <Kore/Graphics4/Graphics.h>
+#include <Kore/Graphics4/Shader.h>
 #include <Kore/System.h>
 
 #include <limits>
@@ -10,28 +10,28 @@
 using namespace Kore;
 
 namespace {
-	Shader* vertexShader;
-	Shader* fragmentShader;
-	Program* program;
-	VertexBuffer* vertices;
-	IndexBuffer* indices;
-	Texture* texture;
-	TextureUnit texunit;
-	ConstantLocation offset;
+	Graphics4::Shader* vertexShader;
+	Graphics4::Shader* fragmentShader;
+	Graphics4::Program* program;
+	Graphics4::VertexBuffer* vertices;
+	Graphics4::IndexBuffer* indices;
+	Graphics4::Texture* texture;
+	Graphics4::TextureUnit texunit;
+	Graphics4::ConstantLocation offset;
 
 	void update() {
-		Graphics::begin();
-		Graphics::clear(Kore::Graphics::ClearColorFlag);
+		Graphics4::begin();
+		Graphics4::clear(Kore::Graphics4::ClearColorFlag);
 
 		program->set();
-		Graphics::setMatrix(offset, mat3::RotationZ((float)Kore::System::time()));
-		Graphics::setVertexBuffer(*vertices);
-		Graphics::setIndexBuffer(*indices);
-		Graphics::setTexture(texunit, texture);
-		Graphics::drawIndexedVertices();
+		Graphics4::setMatrix(offset, mat3::RotationZ((float)Kore::System::time()));
+		Graphics4::setVertexBuffer(*vertices);
+		Graphics4::setIndexBuffer(*indices);
+		Graphics4::setTexture(texunit, texture);
+		Graphics4::drawIndexedVertices();
 
-		Graphics::end();
-		Graphics::swapBuffers();
+		Graphics4::end();
+		Graphics4::swapBuffers();
 	}
 }
 
@@ -39,16 +39,16 @@ int kore(int argc, char** argv) {
 	System::init("TextureTest", 1024, 768);
 	System::setCallback(update);
 
-	texture = new Texture("parrot.png");
+	texture = new Graphics4::Texture("parrot.png");
 
 	FileReader vs("texture.vert");
 	FileReader fs("texture.frag");
-	vertexShader = new Shader(vs.readAll(), vs.size(), VertexShader);
-	fragmentShader = new Shader(fs.readAll(), fs.size(), FragmentShader);
-	VertexStructure structure;
-	structure.add("pos", Float3VertexData);
-	structure.add("tex", Float2VertexData);
-	program = new Program;
+	vertexShader = new Graphics4::Shader(vs.readAll(), vs.size(), Graphics4::VertexShader);
+	fragmentShader = new Graphics4::Shader(fs.readAll(), fs.size(), Graphics4::FragmentShader);
+	Graphics4::VertexStructure structure;
+	structure.add("pos", Graphics4::Float3VertexData);
+	structure.add("tex", Graphics4::Float2VertexData);
+	program = new Graphics4::Program;
 	program->setVertexShader(vertexShader);
 	program->setFragmentShader(fragmentShader);
 	program->link(structure);
@@ -56,14 +56,14 @@ int kore(int argc, char** argv) {
 	texunit = program->getTextureUnit("texsampler");
 	offset = program->getConstantLocation("mvp");
 	
-	vertices = new VertexBuffer(3, structure);
+	vertices = new Graphics4::VertexBuffer(3, structure);
 	float* v = vertices->lock();
 	v[ 0] = -1.0f; v[ 1] = -1.0f; v[ 2] = 0.5f; v[ 3] = 0.0f; v[ 4] = 1.0f;
 	v[ 5] =  1.0f; v[ 6] = -1.0f; v[ 7] = 0.5f; v[ 8] = 1.0f; v[ 9] = 1.0f;
 	v[10] = -1.0f; v[11] =  1.0f; v[12] = 0.5f; v[13] = 0.0f; v[14] = 0.0f;
 	vertices->unlock();
 	
-	indices = new IndexBuffer(3);
+	indices = new Graphics4::IndexBuffer(3);
 	int* i = indices->lock();
 	i[0] = 0; i[1] = 1; i[2] = 2;
 	indices->unlock();
